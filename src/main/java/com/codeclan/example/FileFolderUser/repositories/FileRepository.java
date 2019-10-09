a5a34627d64cd1d2a2a598b1c503b9323ade5435
Kyle <kirvine87@gmail.com>
2019-10-09 09:45:34 +0100

    adds rest repo annotation for interfaces

--- src/main/java/com/codeclan/example/FileFolderUser/repositories/FileRepository.java
+++ src/main/java/com/codeclan/example/FileFolderUser/repositories/FileRepository.java
@@ -2,6 +2,8 @@ package com.codeclan.example.FileFolderUser.repositories;
 
 import com.codeclan.example.FileFolderUser.models.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface FileRepository extends JpaRepository<File, Long> {
 }
--- src/main/java/com/codeclan/example/FileFolderUser/repositories/FolderRepository.java
+++ src/main/java/com/codeclan/example/FileFolderUser/repositories/FolderRepository.java
@@ -2,6 +2,8 @@ package com.codeclan.example.FileFolderUser.repositories;
 
 import com.codeclan.example.FileFolderUser.models.Folder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface FolderRepository extends JpaRepository<Folder, Long> {
 }
